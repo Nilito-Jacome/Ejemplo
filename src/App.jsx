58742e7b2746805808c26185ac95c41cdc2f7266
Nilo-Jacome <nilitojacome@gmail.com>
2024-01-28 12:58:01 -0500

    segundo commit
    adicionando componente home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.css';
+import Home from './componentes/Home';
 
 function App() {
   
   return (
     
       <div>
-        
+        <Home />
       </div>
       
     
