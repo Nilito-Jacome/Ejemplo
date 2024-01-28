266222aaa101603fdfd6413bb59ba5382fd07005
Nilo-Jacome <nilitojacome@gmail.com>
2024-01-28 13:13:33 -0500

    Revert "tercer commit"
    This reverts commit 742d11f3d68b36c15a5b8958235884da407d4f8a.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
 import Home from './componentes/Home';
-import NavBar from './componentes/NavBar';
 
 function App() {
   
   return (
     
       <div>
-        <NavBar />
         <Home />
       </div>
       
