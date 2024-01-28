a9b858e2f6b1e0d85b64f1955054b6ad309ae4e0
Nilo-Jacome <nilitojacome@gmail.com>
2024-01-28 12:19:20 -0500

    Revert "ejemplo"
    This reverts commit 6e9171967f14e12a52fa41036033aadc15ce8e09.

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,17 @@
-import "./App.css";
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import NavBar from './componentes/NavBar';
-import Home from './componentes/Home';
-import About from './componentes/About';
-import Skill from './componentes/Skill';
-import Projects from './componentes/Projects';
-import Certificates from './componentes/Certificates';
-//import Contact from './componentes/Contact';
-//import Tecnology from './componentes/Tecnology';
 
+import './App.css'
 
 function App() {
   
 
   return (
-    <div className="App">
-      <NavBar />
-            
-      <HashRouter>        
-        <Routes>
-        <Route
-            path = "/"
-            element = {<Home/>}            
-          />
-          <Route
-            path = "/about"
-            element = {<About/>}            
-          />          
-          <Route
-            path = "/skill"
-            element = {<Skill/>}            
-          />
-          <Route
-            path = "/projects"
-            element = {<Projects/>}            
-          />  
-          <Route
-            path = "/certificates"
-            element = {<Certificates/>}            
-          />          
-        </Routes>      
-      </HashRouter>
-  
-    </div>
-  );
+    <>
+      <div>
+        
+      </div>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
